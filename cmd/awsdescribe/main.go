5f0cf5c4224dbc1bd29ce8460235be18125274de
sky0621 <sky14work@gmail.com>
2017-05-07 23:20:37 +0900

    command pattern

---
+++ cmd/awsdescribe/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/sky0621/aws-describe-prj/ecr"
+	"github.com/spiegel-im-spiegel/gofacade"
+)
+
+const (
+	// Name はアプリケーション名を定義する
+	Name string = "awsdescribe"
+	// Version はアプリケーションのバージョン番号を定義する
+	Version string = "0.1.0"
+)
+
+func setupFacade(cxt *gofacade.Context) *gofacade.Facade {
+	fcd := gofacade.NewFacade(cxt)
+	fcd.AddCommand(ecr.Name, ecr.Command(cxt, Name))
+	return fcd
+}
+
+func main() {
+	cxt := gofacade.NewContext(os.Stdin, os.Stdout, os.Stderr)
+	fcd := setupFacade(cxt)
+	rtn, err := fcd.Run(Name, Version, os.Args[1:])
+	if err != nil {
+		cxt.Error(fmt.Sprintln(err))
+	}
+	os.Exit(rtn)
+}
---
+++ ecr/ecr.go
@@ -0,0 +1,52 @@
+package ecr
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+
+	"github.com/spiegel-im-spiegel/gofacade"
+)
+
+// Name はコマンド名を定義する
+const Name string = "ecr"
+
+// Context はコマンドのコンテキストを定義する
+type Context struct {
+	//Embedded gofacade.Context
+	*gofacade.Context
+	//AppName にはアプリケーション名を格納する
+	AppName string
+}
+
+// Command は Context のインスタンスを返す
+func Command(cxt *gofacade.Context, appName string) *Context {
+	return &Context{Context: cxt, AppName: appName}
+}
+
+// Synopsis はコマンドの概要を返す
+func (c Context) Synopsis() string {
+	return "Get ECR information"
+}
+
+// Help はコマンドのヘルプを返す
+func (c Context) Help() string {
+	helpText := `
+Usage: awsdescribe ecr
+`
+	return fmt.Sprintln(strings.TrimSpace(helpText))
+}
+
+// Run はコマンドを実行する
+func (c Context) Run(args []string) int {
+	flags := flag.NewFlagSet(Name, flag.ContinueOnError)
+	flags.Usage = func() {
+		c.Error(c.Help())
+	}
+	// Parse commandline flag
+	if err := flags.Parse(args); err != nil {
+		return gofacade.ExitFailure
+	}
+	c.Output("OK ECR!")
+	return gofacade.ExitSuccess
+}
